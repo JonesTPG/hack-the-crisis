56c41ee4bf831b9c3c24c0a77707c049eed9081c
Markus Röman <markus.roman@hotmail.fi>
2020-03-20 20:21:57 +0200

    Added map

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Map from './Map';
 
-import Button from "@material-ui/core/Button";
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Tervetuloa auttamaan pienyrityksiä!</p>
-        <div>
-          <Button variant="contained">Olen asiakas</Button>
-          {"            "}
-
-          <Button variant="contained">Olen yritys</Button>
-        </div>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+        <Map />
       </header>
     </div>
   );
